678fa4bf627dfec92c2a46ea757b6ae451824f3a
Aurel Lossou <aurelth@icloud.com>
2024-11-16 15:52:05 -0300

    Criação da Classe Tarefa

---
+++ GerenciadorDeTarefas/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace GerenciadorDeTarefas
+{
+    public class Tarefa
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; } = false;
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public DateTime? DataConclusao { get; set; } = null;
+    }
+}
